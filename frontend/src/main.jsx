1b30edcd7652232b8bec6782222c0f0c4d889cae
Harsh Sharma <harshsharma20503@gmail.com>
2024-04-20 09:58:27 +0530

    setting up frontend structure

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+if (import.meta.env.DEV) {
+    //for vite application
+    console.log("Running in development mode");
+    axios.defaults.baseURL = import.meta.env.VITE_LOCALHOST;
+} else {
+    console.log("Running in production mode");
+    axios.defaults.baseURL = import.meta.env.VITE_SERVER_URL;
+}
+
+axios.defaults.withCredentials = true;
+
+axios.defaults.ReactDOM.createRoot(document.getElementById("root")).render(
+    <Router>
+        <App />
+    </Router>
+);
