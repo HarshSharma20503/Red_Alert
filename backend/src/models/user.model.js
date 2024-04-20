8a7cc1c8a5c1583c3f86cc69fb111e3d80c4a6d7
Harsh Sharma <harshsharma20503@gmail.com>
2024-04-20 12:49:56 +0530

    added auth routes

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        next();
+    }
+    this.password = await bcrypt.hash(this.password, 12);
+});
+
+export const User = mongoose.model("User", userSchema);
