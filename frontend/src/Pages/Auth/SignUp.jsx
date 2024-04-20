e27ad22fc5814767c03d0db93a5d468323e660b9
Harsh Sharma <harshsharma20503@gmail.com>
2024-04-20 13:45:26 +0530

    added signup page ui

--- frontend/src/Pages/Auth/SignUp.jsx
+++ frontend/src/Pages/Auth/SignUp.jsx
@@ -1,5 +1,56 @@
+import { useState } from "react";
+
 const SignUp = () => {
-    return <div>SignUp</div>;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  return (
+    <>
+      <div className="container h-100 d-flex justify-content-center align-content-center">
+        <form className="d-flex flex-column justify-content-center align-content-center">
+          <h3 className="text-white">Sign In</h3>
+          <div className="mb-3">
+            <label>Full name</label>
+            <input type="text" name="name" className="form-control" placeholder="First name" value={formData.name} />
+          </div>
+          <div className="mb-3">
+            <label>Email address</label>
+            <input
+              type="email"
+              name="email"
+              className="form-control"
+              placeholder="Enter email"
+              value={formData.email}
+            />
+          </div>
+          <div className="mb-3">
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              className="form-control"
+              placeholder="Enter password"
+              value={formData.password}
+            />
+          </div>
+          <div className="d-grid">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default SignUp;
