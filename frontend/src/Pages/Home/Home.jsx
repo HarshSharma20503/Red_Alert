a81d7eb7bda5b9229205d3151b694e0c2dfd7c68
Harsh Sharma <harshsharma20503@gmail.com>
2024-04-20 18:36:45 +0530

    finished getting data

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,19 +1,40 @@
 import AddCompanyModal from "../../Components/Modal/AddCompanyModal";
 import SelectedStocksComponent from "../../Components/List/SelectedStockComponent";
-import Selected from "../../Data/Selected.json";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 const Home = () => {
-  return (
-    <div className="container">
-      <div className="row text-center py-3">
-        <h2 className="text-white">Harsh's Portfolio</h2>
-      </div>
-      <h3 className="row text-white">Company List</h3>
+  const [userInfo, setUserInfo] = useState({});
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const res = await axios.get("/api/user");
+        console.log(res.data.data);
+        setUserInfo(res.data.data);
+      } catch (err) {
+        console.log(err);
+        toast.error(err.response?.data?.message || "Something went wrong");
+      }
+    };
+    fetchUserInfo();
+  }, []);
 
-      <div className="row">
-        <SelectedStocksComponent selectedStocks={Selected} />
+  return (
+    <div className="container ">
+      <div className="row text-center py-3">
+        <h2 className="text-white">{userInfo.name}</h2>
       </div>
-      <div className="row my-3">
+      <h3 className="row text-white mx-2">Company List</h3>
+
+      <div className="row mx-2">
+        {userInfo.companies.length === 0 ? (
+          <h4 className="text-info text-center fs-4">Add Some Companies Stock to view</h4>
+        ) : (
+          <SelectedStocksComponent selectedStocks={userInfo.companies} />
+        )}
+      </div>
+      <div className="row my-3 mx-2">
         <AddCompanyModal />
       </div>
     </div>
